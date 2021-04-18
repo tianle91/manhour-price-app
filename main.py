6772e8ab77264de9c018f4656768a8d5c8038699
Tianle Chen <tianlechen@gmail.com>
2021-04-18 16:48:26 -0400

    unnecessarily long stuffs

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def index():
     price_in_hours = None
     price_in_minutes = None
     if request.method == 'POST':
-        yearly_income = 1000. * float(request.form['yearly-income-in-thousands'])
-        price = float(request.form['price'].strip().lower())
+        yearly_income = 1000. * float(request.form['yearly-k'])
+        price = float(request.form['price'])
         # 262 working days, 8 hours a day
         hourly_income = yearly_income / (262 * 8)
         price_in_hours = price / hourly_income
@@ -19,8 +19,8 @@ def index():
     return render_template(
         'index.html',
         has_price_in_hours=has_price_in_hours,
-        price_in_hours=price_in_hours,
-        price_in_minutes=price_in_minutes
+        price_in_hours=f'{price_in_hours:.2f}' if price_in_hours is not None else None,
+        price_in_minutes=f'{price_in_minutes:.0f}' if price_in_minutes is not None else None,
     )
 
 
