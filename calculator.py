382ab7e811cb66d353858ec187a8d5a78618bcfc
Tianle Chen <tianlechen@gmail.com>
2021-07-15 18:08:29 -0400

    better defaults

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
 def get_income_in_minutes(
-    monthly_income: float, monthly_hours: float = 22.
+    monthly_income: float, monthly_hours: float = 20*7.
 ) -> float:
     return monthly_income / (monthly_hours * 60.)
