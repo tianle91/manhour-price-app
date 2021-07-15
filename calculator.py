de56ee2c8372da9686903bac8b384341fce73504
Tianle Chen <tianlechen@gmail.com>
2021-07-15 16:48:32 -0400

    lint

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 def get_income_in_minutes(
-    monthly_income: float, 
-    monthly_hours: float = 22. # 262 working days, 8 hours a day
+    monthly_income: float, monthly_hours: float = 22.
 ) -> float:
     return monthly_income / (monthly_hours * 60.)
